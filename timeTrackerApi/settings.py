e31e2943f2943e7beb0132815ac96bd4849bf038
abdelrahman84 <abdelrahman.farag84@gmail.com>
2025-12-08 20:31:50 +0200

    continue fixes to semaphore CI

--- timeTrackerApi/settings.py
+++ timeTrackerApi/settings.py
@@ -33,8 +33,8 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
 
-CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", "").split(",")
-CORS_ORIGIN_WHITELIST = os.environ.get("DJANGO_CORS_ORIGIN_WHITELIST", "").split(",")
+CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+CORS_ORIGIN_WHITELIST = os.environ.get("DJANGO_CORS_ORIGIN_WHITELIST", "http://localhost:3000").split(",")
 
 # Application definition
 
